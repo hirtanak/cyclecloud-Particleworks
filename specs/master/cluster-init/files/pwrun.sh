c9ab1e36b4ce94c471e5c5d7d50c05c139f819c2
hirtanak <hirtanak@gmail.com>
2019-11-19 08:15:20 +0900

    remove sample script

--- specs/master/cluster-init/files/pwrun.sh
+++ specs/master/cluster-init/files/pwrun.sh
@@ -17,18 +17,3 @@ source /opt/intel/impi/2018.4.274/bin64/mpivars.sh
 #export I_MPI_FABRICS=shm:dapl
 #export I_MPI_DAPL_PROVIDER=ofa-v2-ib0
 #export I_MPI_DYNAMIC_CONNECTION=0
-
-#Particleworks
-#mpirun -np プロセス数 /path/to/module/app.solver.double -p scene -k simd -n プロセス当たりスレッド数 --job
-#GPUの場合（8分程度）
-#/path/to/module/app.solver.double -p scene -k cuda -g 0 -n 1
-
-#Granuleworks
-#CPUの場合
-#/path/to/module/app.solver.double -p scene -k simd -n スレッド数
-#GPUの場合
-#/path/to/module/app.solver.double -p scene -k cuda -g 0 -n 1
-
-cd ${HOME}/apps/prj # project directory sample. You have to change your project directory.
-
-/opt/intel/impi/2018.4.274/bin64/mpirun -np ${NP} ${HOME}/apps/pw-linux-package/module/app.solver.double -p scene -k simd -n 1 --job 2>&1 | tee PWlog-`date +%Y%m%d_%H-%M-%S`.log
